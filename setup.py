0cb583a401939ee3a8e20c88004deca86d9a5e1b
subhash kumar singh <subh.singh@samsung.com>
2025-09-15 15:05:34 +0530

    Default pipeline image build failure
    Fixed package dependency to avoid package conflict.
    
    Issue-ID: AIMLFW-244
    Change-Id: Id02a51c7c80b4769cff479b2575af104e154c69b
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>

--- setup.py
+++ setup.py
@@ -19,13 +19,18 @@
 from setuptools import setup
 setup(
     name='featurestoresdk',
-    version='0.3',
+    version='0.3.2',
     description='feature store SDK for Training Host',
     url='https://gerrit.o-ran-sc.org/r/admin/repos/aiml-fw/athp/sdk/feature-store,general',
-    author='Joseph Thaliath',
-    author_email='jo.thaliath@samsung.com',
+    author='O-RAN Software Community',
+    author_email='discuss@lists.o-ran-sc.org',
     license='Apache 2.0',
     packages=['featurestoresdk'],
     package_data={"featurestoresdk": ['config/config.json']},
-    zip_safe=False
+    install_requires=[
+        "pandas==1.4.4",
+        "cassandra-driver==3.25.0",
+        "kubernetes",
+    ],
+    zip_safe=False,
 )
